1201d4f12dc6d8341c345a6dd3ba6bc7c4b5eef9
Atri Thakar <atrithaker122@gmail.com>
2024-08-31 18:50:36 +0530

    fixed mistake

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.confg['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class MyTask(db.Model):
